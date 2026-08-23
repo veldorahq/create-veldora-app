56e827069a3fdef119151ac18d4643db772b56f1
Veldora <modraostudio@gmail.com>
2026-08-23 21:49:39 +0600

    template: add favicon.svg and favicon link tag to default layout
    - Copy Veldora transparent SVG favicon to template/public/favicon.svg
    - Add <link rel='icon' href='/favicon.svg'> to default app layout
      so all new projects scaffolded with create-veldora-app get the real favicon

--- template/resources/views/layouts/app.veldora.php
+++ template/resources/views/layouts/app.veldora.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ $title ?? 'Veldora' }}</title>
-
+    <link rel="icon" href="/favicon.svg" type="image/svg+xml">
     <!-- Google Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
